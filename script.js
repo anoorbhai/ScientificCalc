30a08eedd15a0f904b698257e6242c5b2bd1ea2b
anoorbhai <100721161+anoorbhai@users.noreply.github.com>
2023-12-04 03:08:06 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,88 @@
+var screen = document.querySelector('#screen');
+var btn = document.querySelectorAll('.btn');
+
+for (item of btn) {
+    item.addEventListener('click', (e) => {
+        btntext = e.target.innerText;
+
+        if (btntext == '×') {
+            btntext = '*';
+        }
+
+        if (btntext == '÷') {
+            btntext = '/';
+        }
+        screen.value += btntext;
+    });
+}
+
+document.getElementById('theme-toggle').addEventListener('change', function(event) {
+  if (event.target.checked) {
+    document.body.classList.remove('light-mode');
+    document.body.classList.add('dark-mode');
+  } else {
+    document.body.classList.remove('dark-mode');
+    document.body.classList.add('light-mode');
+  }
+});
+
+function sin() {
+    screen.value = Math.sin(screen.value);
+}
+
+function cos() {
+    screen.value = Math.cos(screen.value);
+}
+
+function tan() {
+    screen.value = Math.tan(screen.value);
+}
+
+function pow() {
+    screen.value = Math.pow(screen.value, 2);
+}
+
+function sqrt() {
+    screen.value = Math.sqrt(screen.value, 2);
+}
+
+function log() {
+    screen.value = Math.log(screen.value);
+}
+
+function pi() {
+    screen.value = 3.14159265359;
+}
+
+function e() {
+    screen.value = 2.71828182846;
+}
+
+function fact() {
+    var i, num, f;
+    f = 1
+    num = screen.value;
+    for (i = 1; i <= num; i++) {
+        f = f * i;
+    }
+
+    i = i - 1;
+
+    screen.value = f;
+}
+
+function calculate() {
+    let screen = document.getElementById('screen'); // assuming 'screen' is the id of your input field
+    try {
+        let result = new Function('return ' + screen.value)();
+        screen.value = result;
+    } catch (error) {
+        console.error(error);
+        alert('Invalid calculation');
+    }
+}
+
+function backspc() {
+    screen.value = screen.value.substr(0, screen.value.length - 1);
+}
+
